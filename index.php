21019f36f136b0cbc531a19187fc52f9a794b43e
jenn <epistemicpolymathic5@gmail.com>
2017-04-06 14:54:48 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+echo "<br/><br/><br/><br/><br/>";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <title>My News Site</title>
+    </head>
+    <!--[if IE 6 ]><body class="ie6 old_ie"><![endif]-->
+    <!--[if IE 7 ]><body class="ie7 old_ie"><![endif]-->
+    <!--[if IE 8 ]><body class="ie8"><![endif]-->
+    <!--[if IE 9 ]><body class="ie9"><![endif]-->
+    <!--[if !IE]><!--><body><!--<![endif]-->
+		<header>
+			<h1><a href="index.php">Box Press</a><span id="version">v1</span></h1>
+			<nav>
+				<ul>
+					<li><a href="index.php" class="current">Home</a></li>
+					<li><a href="#">My Favourite</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="secwrapper">
+			<section>
+					<article>
+						<a href="#"><img src="https://static01.nyt.com/images/misc/NYT_logo_rss_250x40.png" alt=""/></a>
+						<h1>Title</h1>
+						<p>Description</p>
+						<a href="#" class="readmore">Read more</a>
+						<p style="color:blue;">1/1/2001</p>
+					</article>
+			</section>
+		</div>
+		<footer>
+			<p>Copyright &copy 2012 BoxPress by Youssef Nassim. All Rights Reserved.</p>
+		</footer>
+	</body>
+</html>
+		
+		
+			
+    
+    
